710ba196632e8f247cf1cb91c21cc6951ba11877
Jonek <jonek@gmx.net>
2020-06-18 00:59:22 +0200

    Add script to download data from MaStR

---
+++ data/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -nc -O DatenAb310119.zip https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Energie/Unternehmen_Institutionen/ErneuerbareEnergien/ZahlenDatenInformationen/VOeFF_Registerdaten/DatenAb310119.zip?__blob=publicationFile&v=8
+wget -nc -O MaStRDatenMigration1.zip https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Energie/Unternehmen_Institutionen/ErneuerbareEnergien/ZahlenDatenInformationen/VOeFF_Registerdaten/MaStRDatenMigration1.zip?__blob=publicationFile&v=3
+wget -nc -O MaStRDatenMigration2.zip https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Energie/Unternehmen_Institutionen/ErneuerbareEnergien/ZahlenDatenInformationen/VOeFF_Registerdaten/MaStRDatenMigration2.zip?__blob=publicationFile&v=3
